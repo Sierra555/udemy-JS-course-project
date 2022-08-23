cae07ca5c8bce40e60b1c1843705c51df4a15afd
Dara Stotska <dara6234@gmail.com>
2022-08-23 14:31:19 +0300

    Second commit

--- js/script.js
+++ js/script.js
@@ -227,5 +227,46 @@ window.addEventListener('DOMContentLoaded', ()=> {
             12,
             ".menu .container",
     ).render();
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: "Завантаження",
+        success: "Дякую, незабаром ми з вами зв'яжимося",
+        failure: "Щось пішло не так..."
+    };
+    forms.forEach(item => {
+        postData(item);
+    });
+    function postData(form){
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const statusMessage = document.createElement('div');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+    request.setRequestHeader("Content-type", "application/json");
+
+        const formData = new FormData(form);
+        const obj = {};
+        formData.forEach((value,key) => {
+            obj.key = value;
+        });
+        const json = JSON.stringify(obj);
+        request.send(json);
+        request.addEventListener("load", () => {
+            if (request.status === 200) {
+                statusMessage.textContent = message.success;
+                console.log(request.response);
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 3000);
+            }
+            else { statusMessage.textContent = message.failure; }
+        });
+
+    });
     
 });
